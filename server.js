15d0b9dc34474bed486052278dae99cf0ed3debf
Otavio Oliveira <otavio.oliveira@sou.unifeob.edu.br>
2018-06-05 21:20:43 -0300

    alteracao de teste

--- server.js
+++ server.js
@@ -4,5 +4,5 @@ const port =  process.env.PORT || 3000
 let server = app.listen(port, () => {
   let host = server.address().address
   let port = server.address().port
-  console.log("Moto app listening at http://%s:%s", host, port)
+  console.log("Moto API app listening at http://%s:%s", host, port)
 })
